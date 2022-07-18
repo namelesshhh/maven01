3d632820960d7f578897688c4e8d85168a0196b0
dzq <2273506711@qq.com>
2022-07-18 22:53:27 +0800

    third commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,7 +3,8 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git2");
         System.out.println("hello git3 update by hot-fix");
+        System.out.println("hello git3 update by hot-fix");
+        System.out.println("hello");
     }
 }
